9e7a6d369c61f753abb624ad7159db19b1e11bc3
khasnaradak <33849991+S686@users.noreply.github.com>
2023-05-09 01:28:29 +0900

    [Silver III] Title: 프린터 큐, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+testcase = int(input())
+for i in range(testcase):
+    n, m = map(int, input().split())
+    queue = list(map(int, input().split()))
+    queue = list((i, idx) for idx, i in enumerate(queue))
+
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == m:
+                print(count)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
